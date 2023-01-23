c54f82d6ffcf4daa68b7d785d3586e657aab0704
eniolajayi <ajayieniola16@gmail.com>
2023-01-23 23:29:51 +0100

    add logic for Player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private CharacterController character;
+    private Vector3 direction;
+
+    public float gravity = 9.81f * 2f;
+    public float jumpForce = 8f;
+
+    private void Awake()
     {
-        
+        character = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        direction = Vector3.zero;
     }
+
+    private void Update()
+    {
+        direction += Vector3.down * gravity * Time.deltaTime;
+
+        if (character.isGrounded)
+        {
+            direction = Vector3.down;
+
+            if (Input.GetButton("Jump"))
+            {
+                direction = Vector3.up * jumpForce;
+            }
+        }
+
+        character.Move(direction * Time.deltaTime);
+    }
+
+
 }
