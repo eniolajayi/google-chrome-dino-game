f6106a66154729f4f6fd6ccf50c6c4eb8560ff91
eniolajayi <ajayieniola16@gmail.com>
2023-01-24 11:28:13 +0100

    make ground scroll based on gameSpeed

--- Assets/Scripts/Ground.cs
+++ Assets/Scripts/Ground.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 
 public class Ground : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private MeshRenderer meshRenderer;
+
+    private void Awake()
     {
-        
+        meshRenderer = GetComponent<MeshRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float speed = GameManager.Instance.gameSpeed / transform.localScale.x;
+        meshRenderer.material.mainTextureOffset += Vector2.right * speed * Time.deltaTime;
     }
 }
