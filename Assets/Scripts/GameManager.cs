8bb782e376efd8fce76583c4a1fa6ac63c235775
eniolajayi <ajayieniola16@gmail.com>
2023-02-01 21:55:53 +0100

    add gameover functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,10 @@ public class GameManager : MonoBehaviour
     public float gameSpeedIncrease = 0.1f;
     public float gameSpeed { get; private set; }
 
+    private Player player;
+    private Spawner spawner;
+
+
     private void Awake()
     {
         if (Instance == null)
@@ -30,12 +34,33 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        player = FindObjectOfType<Player>();
+        spawner = FindObjectOfType<Spawner>();
         NewGame();
     }
 
     private void NewGame()
     {
+        Obstacle[] obstacles = FindObjectsOfType<Obstacle>();
+        foreach (Obstacle obstacle in obstacles)
+        {
+            Destroy(obstacle.gameObject);
+        }
+
         gameSpeed = initialGameSpeed;
+        enabled = true;
+
+        player.gameObject.SetActive(true);
+        spawner.gameObject.SetActive(true);
+    }
+
+    public void GameOver()
+    {
+        gameSpeed = 0;
+        enabled = false;
+
+        player.gameObject.SetActive(false);
+        spawner.gameObject.SetActive(false);
     }
 
     private void Update()
